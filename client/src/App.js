16aa869b33da4b4d09f12a063bc9456a82bfa94a
Mikael <dev.kov.mtl@gmail.com>
2022-06-11 14:52:46 -0400

    Client - Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,29 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 import Clients from './components/Clients';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:8080/graphql',
-  cache: new InMemoryCache(),
+  // cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
